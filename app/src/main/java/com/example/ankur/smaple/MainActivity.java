5ef72b8ff637b46d2cead82fc2ca63516eb88184
ankur <ankurj630@gmail.com>
2018-02-18 15:26:54 +0530

    loader

--- app/src/main/java/com/example/ankur/smaple/MainActivity.java
+++ app/src/main/java/com/example/ankur/smaple/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.ankur.smaple;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.ankur.smaple.utilities.NetworkUtilities;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     EditText mSearchBoxEditText;
     TextView mUrlDisplay;
     TextView mSearchResults;
+    TextView errorMessageTextView;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,39 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = findViewById(R.id.et_search_box);
         mUrlDisplay = findViewById(R.id.tv_url_display);
         mSearchResults = findViewById(R.id.tv_github_search_result_json);
+        errorMessageTextView = findViewById(R.id.tv_error_display);
+        mProgressBar = findViewById(R.id.pb_loading_indicator);
+
+    }
+
+     public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+         @Override
+         protected void onPreExecute() {
+             super.onPreExecute();
+             mProgressBar.setVisibility(View.VISIBLE);
+         }
+
+         @Override
+        protected String doInBackground(URL... urls) {
+            URL searchUrl = urls[0];
+            String githubSearchResult = null;
+            try {
+                githubSearchResult = NetworkUtilities.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return githubSearchResult;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+             mProgressBar.setVisibility(View.INVISIBLE);
+            if (s != null && !s.equals("")) {
+                mSearchResults.setText(s);
+            }
+        }
     }
 
     @Override
@@ -34,10 +77,29 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuItemThatWasSelected = item.getItemId();
         if (menuItemThatWasSelected == R.id.action_search) {
-            Context context = MainActivity.this;
-            String message = "Search Clicked";
-            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+            makeGithubSearchQuery();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    void makeGithubSearchQuery(){
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchUrl = NetworkUtilities.buildUrl(githubQuery);
+        if (githubSearchUrl == null) {
+            mUrlDisplay.setText("Please try again");
+        } else {
+            mUrlDisplay.setText(githubSearchUrl.toString());
+            new GithubQueryTask().execute(githubSearchUrl);
+        }
+    }
+
+    private void showDataJsonView() {
+        errorMessageTextView.setVisibility(View.INVISIBLE);
+        mSearchResults.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        errorMessageTextView.setVisibility(View.VISIBLE);
+        mSearchResults.setVisibility(View.INVISIBLE);
     }
 }
---
+++ app/src/main/java/com/example/ankur/smaple/utilities/NetworkUtilities.java
@@ -0,0 +1,50 @@
+package com.example.ankur.smaple.utilities;
+
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtilities {
+    final static String GITHUB_BASE_URL = "https://api.github.com/search/repositories";
+    final static String PARAM_QUERY = "q";
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    public static URL buildUrl(String githubSearchQuery){
+        Uri buildUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_QUERY, sortBy).build();
+        try {
+            return new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
